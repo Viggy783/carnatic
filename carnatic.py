c50728596cc2f4e799fabe7447e5be5a5a71adce
Vignesh Vanchinathan <viggy.vanchi@gmail.com>
2022-08-24 19:29:03 -0400

    added thaalam list

--- carnatic.py
+++ carnatic.py
@@ -1,8 +1,10 @@
 class Carnatic:
 
-    def __init__(self, aro, avaro):
+    def __init__(self, aro, avaro, tha):
         self.aro = aro
         self.avaro = avaro
+        self.thaalam = tha
+        self.printThaalam(tha)
 
     def test(self):
         print(self.aro)
@@ -15,6 +17,7 @@ class Carnatic:
             print("||\n")  
     
     def descending(self, pattern):
+        print("---")
         for i in range(5):
             for j in range(len(pattern)):
                 print(self.avaro[i + (pattern[j] - 1)], end = " ")
@@ -24,28 +27,29 @@ class Carnatic:
         self.ascending(pattern)
         self.descending(pattern)
 
-    def thaalam3(self): # Chathurasha jaathi rupaka thaalam
-        pattern = [1, 2, 1, 2, 3, 4]
-        self.scale(pattern)
+    def printThaalam(self, thaalam):
+        thaalamList = {
+            "0": None,
+            "1": [1, 2, 3, 4, 3, 2, 1, 2, 3, 2, 1, 2, 3, 4], #chaturasrajaati dhruva thaalam
+            "2": [1, 2, 3, 2, 1, 2, 1, 2, 3, 4], #chathurasra jaati matya thaalam
+            "3": [1, 2, 1, 2, 3, 4],
+            "4": [1, 2, 3, 1, 2, 1, 2, 3, 4],
+            "5": [1, 2, 3, 1, 2, 3, 4],
+            "6": None,
+            "7": [1, 2, 3, 4],
+            "8": None,
+        }
 
-    def thaalam4(self): #misra jaathi jhumpa thaalam
-        pattern = [1, 2, 3, 1, 2, 1, 2, 3, 4]
-        self.scale(pattern)  
-    
-    def thaalam5(self):
-        pattern = [1, 2, 3, 1, 2, 3, 4]
-        self.scale(pattern)
-        
+        self.scale(thaalamList[thaalam])
+
     def customThaalam(self):
-        pattern = input("\ninput thaalam pattern (e.g. 1 2 3 1 2 1 2 3 4\t").split()
+        pattern = input("\ninput thaalam pattern (e.g. 1 2 3 1 2 1 2 3 4)\t").split()
         self.scale(pattern)
     
 
 #assumption that avarohanam is reverse of arohanam
 arohanam = input("\n\n\n----\ninput arohanam (e.g. S R G M P D N S)\t").split()
 avarohanam = arohanam[::-1]
-
-carnatic = Carnatic(arohanam, avarohanam)
-carnatic.thaalam4()
-print("\n\n\n")
-carnatic.thaalam3()
+print(avarohanam)
+thaalam = input('\ninput thaalam number (e.g. 4)\t')
+carnatic = Carnatic(arohanam, avarohanam, tha=thaalam)
